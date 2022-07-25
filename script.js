c44e0246768c6ff2aca3173228b0cd93485ea503
HErikH <erik.harutyunyan.03.03@mail.ru>
2022-07-25 18:18:39 +0400

    second

--- script.js
+++ script.js
@@ -3,88 +3,120 @@ let playerScore = 0;
 let computerScore = 0;
 
 // buttons
-let rock = document.getElementById('rock')
-let scissor = document.getElementById('scissor')
-let paper = document.getElementById('paper')
+let rock = document.getElementById("rock");
+let scissor = document.getElementById("scissor");
+let paper = document.getElementById("paper");
 // score
-let movesLeft = document.getElementById('movesLeft')
-let computerCount = document.getElementById('cCount')
-let playerCount = document.getElementById('pCount')
-// 
-let computerChoose = ['rock', 'scissor', 'paper']
+let table = document.getElementById("table");
+let movesLeft = document.getElementById("movesLeft");
+let computerCount = document.getElementById("cCount");
+let playerCount = document.getElementById("pCount");
+//
+let computerChoose = ["rock", "scissor", "paper"];
 
-window.onload = function() {
-    function run(playerChoose) {
-        move--
-        movesLeft.innerText = move
+window.onload = function () {
 
-        let result = Math.floor(Math.random() * 3)
+  function run(playerChoose) {
 
-        if(playerChoose == computerChoose[result]){
-            console.log ('Draw!')
-        }
-        else if (playerChoose == 'rock') {
-            if (computerChoose[result] == 'paper') {
-                computerScore++
-                computerCount.innerText = computerScore
-                console.log ('Computer won!')
-            } else {
-                playerScore++
-                playerCount.innerText = playerScore
-                console.log('Player won!')
-            } 
-        }  else if (playerChoose == 'scissor') {
-            if (computerChoose[result] == 'rock') {
-                computerScore++
-                computerCount.innerText = computerScore
-                console.log('Computer won!')
-            } else {
-                playerScore++
-                playerCount.innerText = playerScore
-                console.log('Player won!')
-            }
-        }  else if (playerChoose == 'paper') {
-            if (computerChoose[result] == 'scissor') {
-                computerScore++
-                computerCount.innerText = computerScore
-                console.log('Computer won!')
-            } else {
-                playerScore++
-                playerCount.innerText = playerScore
-                console.log('Player won!')
-            }
-        }
+    move--;
+    movesLeft.innerText = move;
 
-        if (move == 0) {
-            if (playerScore > computerScore) {
-                console.log(`Player won with ${playerScore} score !`)
-                move = 10
-                playerScore = 0
-                computerScore = 0
-                movesLeft.innerText = move
-                playerCount.innerText = playerScore
-                computerCount.innerText = computerScore
-            } else {
-                console.log(`Computer won with ${computerScore} score !`)
-                move = 10
-                playerScore = 0
-                computerScore = 0
-                movesLeft.innerText = move
-                playerCount.innerText = playerScore
-                computerCount.innerText = computerScore
-            }
-        }
+    let result = Math.floor(Math.random() * 3);
 
-    }
-    
-    rock.onclick = function() {
-        run('rock')
-    }
-    scissor.onclick = function() {
-        run('scissor')
-    }
-    paper.onclick = function() {
-        run('paper')
+    if (playerChoose == computerChoose[result]) {
+      table.innerText = "Draw !";
+      table.style.display = "flex";
+      setTimeout(() => {
+        table.style.display = "none";
+      }, 1000);
+    } else if (playerChoose == "rock") {
+      if (computerChoose[result] == "paper") {
+        computerScore++;
+        computerCount.innerText = computerScore;
+        table.innerText = "Computer won !";
+        table.style.display = "flex";
+        setTimeout(() => {
+          table.style.display = "none";
+        }, 1000);
+      } else {
+        playerScore++;
+        playerCount.innerText = playerScore;
+        table.innerText = "Player won !";
+        table.style.display = "flex";
+        setTimeout(() => {
+          table.style.display = "none";
+        }, 1000);
+      }
+    } else if (playerChoose == "scissor") {
+      if (computerChoose[result] == "rock") {
+        computerScore++;
+        computerCount.innerText = computerScore;
+        table.innerText = "Computer won!";
+        table.style.display = "flex";
+        setTimeout(() => {
+          table.style.display = "none";
+        }, 1000);
+      } else {
+        playerScore++;
+        playerCount.innerText = playerScore;
+        table.innerText = "Player won !";
+        table.style.display = "flex";
+        setTimeout(() => {
+          table.style.display = "none";
+        }, 1000);
+      }
+    } else if (playerChoose == "paper") {
+      if (computerChoose[result] == "scissor") {
+        computerScore++;
+        computerCount.innerText = computerScore;
+        table.innerText = "Computer won !";
+        table.style.display = "flex";
+        setTimeout(() => {
+          table.style.display = "none";
+        }, 1000);
+      } else {
+        playerScore++;
+        playerCount.innerText = playerScore;
+        table.innerText = "Player won !";
+        table.style.display = "flex";
+        setTimeout(() => {
+            table.style.display = "none";
+        }, 1000);
+      }
     }
 
-}
+    if (move == 0) {
+      if (playerScore > computerScore) {
+        table.innerText = `Player won with ${playerScore} score !`
+        table.style.display = "flex";
+        setTimeout(() => {
+            table.style.display = "none";
+        }, 1000);
+        move = 10;
+        playerScore = 0;
+        computerScore = 0;
+        movesLeft.innerText = move;
+        playerCount.innerText = playerScore;
+        computerCount.innerText = computerScore;
+      } else {
+        table.innerText = `Computer won with ${computerScore} score !`
+        move = 10;
+        playerScore = 0;
+        computerScore = 0;
+        movesLeft.innerText = move;
+        playerCount.innerText = playerScore;
+        computerCount.innerText = computerScore;
+      }
+    }
+  }
+
+  rock.onclick = function () {
+    run("rock");
+  };
+  scissor.onclick = function () {
+    run("scissor");
+  };
+  paper.onclick = function () {
+    run("paper");
+  };
+};
